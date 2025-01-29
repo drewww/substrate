6e5d467fd559d90134c0843ed93aef8ac33766ec
Drew Harry <drew.harry@gmail.com>
2025-01-29 11:19:40 -0800

    "Shooting" sort of works.

--- src/game/test/systems/enemy-ai.system.ts
+++ src/game/test/systems/enemy-ai.system.ts
@@ -2,6 +2,8 @@ import { World } from '../../../world/world';
 import { ActionHandler } from '../../../action/action-handler';
 import { Entity } from '../../../entity/entity';
 import { logger } from '../../../util/logger';
+import { EnemyAIComponent } from '../components/enemy-ai.component';
+import { SymbolComponent } from '../../../entity/components/symbol-component';
 
 export class EnemyAISystem {
     constructor(
@@ -21,12 +23,28 @@ export class EnemyAISystem {
     }
 
     private updateEnemy(enemy: Entity): void {
-        const ai = enemy.getComponent('enemyAI');
+        const ai = enemy.getComponent('enemyAI') as EnemyAIComponent;
 
         const canSeePlayer = this.world.canEntitySeeEntity(enemy, this.world.getPlayer());
 
         if(canSeePlayer) {
-            logger.warn(`Enemy at ${enemy.getPosition().x}, ${enemy.getPosition().y} can see player at ${this.world.getPlayer().getPosition().x}, ${this.world.getPlayer().getPosition().y}`);
+            ai.turnsLocked+=1
+            // logger.warn(`Enemy at ${enemy.getPosition().x}, ${enemy.getPosition().y} can see player at ${this.world.getPlayer().getPosition().x}, ${this.world.getPlayer().getPosition().y}`);
+
+            if(ai.turnsLocked > 6) {
+                // fire a "projectile"
+
+                // for now let's just add an entity at the player's position
+                const projectile = new Entity(this.world.getPlayer().getPosition());
+                projectile.setComponent(new SymbolComponent('*', '#FFFFFFff', '#000000ff', 1500));
+                this.world.addEntity(projectile);
+
+                ai.turnsLocked = 0;
+            }
+        } else {
+            ai.turnsLocked = 0;
         }
+
+        enemy.setComponent(ai);
     }
 } 
