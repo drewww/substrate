b19e5e94e20cc3edd1d2657e5eb491f3e9405025
Drew Harry <drew.harry@gmail.com>
2025-03-03 16:47:34 -0800

    Renaming 4-6i to match pattern. Loading the blocks out and then randomly dropping them into the world.

--- src/game/generators/city-block-generator.ts
+++ src/game/generators/city-block-generator.ts
@@ -43,35 +43,41 @@ export class CityBlockGenerator {
                 try {
                     // Get the block file URL from the imported files
                     // always use 4-4i for now. we will diversify later.
-                    // const blockUrl = await blockFiles['../../assets/blocks/4-4i.json']();
+                    // choose between 4-4i and 4-6i randomly. those are our two options now.
+                    let blockUrl;
+                    if(Math.random() > 0.5) {
+                        blockUrl = await blockFiles['../../assets/blocks/4-4i.json']();
+                    } else {
+                        blockUrl = await blockFiles['../../assets/blocks/4-6i.json']();
+                    }
                     
-                    // if (!blockUrl) {
-                    //     logger.error(`No block file found at 4-4i.json`);
-                    //     continue;
-                    // }
+                    if (!blockUrl) {
+                        logger.error(`No block file found at 4-4i.json`);
+                        continue;
+                    }
 
-                    // const blockGenerator = await JsonWorldGenerator.fromUrl(blockUrl);
-                    // const blockWorld = blockGenerator.generate();
+                    const blockGenerator = await JsonWorldGenerator.fromUrl(blockUrl);
+                    const blockWorld = blockGenerator.generate();
 
-                    // // Copy entities from block to city world with offset
-                    // const offsetX = x * this.blockWidth;
-                    // const offsetY = y * this.blockHeight;
+                    // Copy entities from block to city world with offset
+                    const offsetX = x * this.blockWidth;
+                    const offsetY = y * this.blockHeight;
 
-                    // blockWorld.getEntities().forEach(entity => {
-                    //     const pos = entity.getPosition();
-                    //     const newEntity = entity.clone();
-                    //     newEntity.setPosition(pos.x + offsetX, pos.y + offsetY);
-                    //     world.addEntity(newEntity);
-                    // });
+                    blockWorld.getEntities().forEach(entity => {
+                        const pos = entity.getPosition();
+                        const newEntity = entity.clone();
+                        newEntity.setPosition(pos.x + offsetX, pos.y + offsetY);
+                        world.addEntity(newEntity);
+                    });
 
                     // just fill in the entire block with
 
                     // just iterate from 0,0 in block coordinates to 11,11
-                    for (let i = 0; i < this.blockWidth; i++) {
-                        for (let j = 0; j < this.blockHeight; j++) {
-                            this.placeRoad(i+x*this.blockWidth, j+y*this.blockHeight, world);
-                        }
-                    }
+                    // for (let i = 0; i < this.blockWidth; i++) {
+                    //     for (let j = 0; j < this.blockHeight; j++) {
+                    //         this.placeRoad(i+x*this.blockWidth, j+y*this.blockHeight, world);
+                    //     }
+                    // }
 
                     // logger.info(`Loaded block at ${x},${y} with ${blockWorld.getEntities().length} entities`);
                 } catch (error) {
